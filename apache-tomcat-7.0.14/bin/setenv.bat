b47391e0561a21fc015a6c3e0b1b692f9460025d
ccmtaylor <ccmtaylor@collab.net>
2011-12-13 11:31:53 +0000

    copy current version of tomcat distribution and dummy service to trunk for future development
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk/ccf-tomcat7@5207 6771e1e2-0f46-0410-b5d7-d846ff89f743

---
+++ apache-tomcat-7.0.14/bin/setenv.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set JAVA_OPTS=%JAVA_OPTS% -Xms512m -Xmx1024m
+rem voodoo parameters to try to avoid OutOfMemoryErrors on webapp redeployment
+set JAVA_OPTS=%JAVA_OPTS% -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m
+
+set CCF_CONF="%CATALINA_HOME%\ccf.conf"
+if exist %CCF_CONF% (
+	echo setting up CCF system properties
+	for /f "usebackq tokens=1,2 eol=# delims==" %%a in (%CCF_CONF%) do (
+		echo setting %%a to %%b
+		call set JAVA_OPTS=%%JAVA_OPTS%% "-D%%a=%%b"
+	)
+) else (
+	echo WARNING: could not find CCF configuration file: %CCF_CONF%
+)
+echo JAVA_OPTS: %JAVA_OPTS%
+
