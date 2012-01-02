0e496f230aa4864952d3c7dce83ab20bfd4c1aa9
jonico <jnicolai@collab.net>
2012-01-02 14:26:11 +0000

    Renamed CCF 2.x branch to indicate that we have reintegrated all sub directories into trunk again
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/branches/CCF%202.x%20-%20Moved%20back%20into%20trunk/ccf-tomcat7@5245 6771e1e2-0f46-0410-b5d7-d846ff89f743

---
+++ dist.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# this is just the commands from CREATE_DISTRIBUTION.txt for convenience.
+
+svn export https://ctf.open.collab.net/svn/repos/ccf/branches/CCF%202.x/ccf-tomcat7/apache-tomcat-7.0.14
+wget \
+    http://bit.ly/ccfmaster_last_successful_build \
+    -O apache-tomcat-7.0.14/webapps/CCFMaster.war
+tar czf ccf-tomcat.tar.gz apache-tomcat-7.0.14
+zip -r9q ccf-tomcat.zip apache-tomcat-7.0.14
